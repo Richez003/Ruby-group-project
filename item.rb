f2d290dabe36efc4a86b04c7f49e37b6417a3dd1
richemonday@gmail.com <richemonday@gmail.com>
2022-08-25 11:47:24 +0100

    we decleared the methods for genre lable and achieved

--- item.rb
+++ item.rb
@@ -7,3 +7,30 @@ class Item
         @archived = false
         @published_date = published_date
       end
+
+      def add_genre=(genre)
+        @genre = genre  
+        author.items.push(self) unless author.items.include?(self)
+      end
+
+      def add_genre=(genre)
+        @genre = genre
+        genre.items.push(self) unless genre.items.include?(self)
+      end
+    
+      def add_label=(label)
+        @label = label
+        label.items.push(self) unless label.items.include?(self)
+      end
+
+      def move_to_archive
+        @archived = true if can_be_archived?
+      end
+    
+      private
+    
+      def can_be_archived?
+        Date.new.year - @published_date > 10
+      end
+    end
+    
