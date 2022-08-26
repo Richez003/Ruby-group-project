b5fe2e3d5cdbf92aeb4c266d356c3493c0c97104
richemonday@gmail.com <richemonday@gmail.com>
2022-08-26 15:59:16 +0100

    added test file for book spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,33 +1,19 @@
-require './book'
+require_relative '../book'
 
 describe Book do
   context 'test the methods in the Book class' do
-    book = Book.new('The History of Tom Jones', 'Henry Fielding', 'good', '1749')
+    book = Book.new('History of Tom Jones', 'Henry Fielding', 'good', '1749')
     it 'should return the correct title' do
-      expect(book.title).to eq('The History of Tom Jones')
+      expect(book.title).to eq('History of Tom Jones')
     end
+
+    it 'test if can_be_archived? returns true' do
+      expect(book.move_to_archive).to eq(true)
+    end
+
+    it 'test if can_be_archived? returns true' do
+      book.move_to_archive
+      expect(book.archived).to eq(true)
+    end
+  end
 end
-end
-
-    # it 'should return the correct publisher' do
-    #   expect(book.publisher).to eq('George Allen & Unwin (UK)')
-    # end
-
-    # it 'should return the correct cover state' do
-    #   expect(book.cover_state).to eq('good')
-    # end
-
-    # it 'should return the correct publish date' do
-    #   expect(book.publish_date).to eq('1937')
-#     end
-
-#     it 'test if can_be_archived? returns true' do
-#       expect(book.move_to_archive).to eq(true)
-#     end
-
-#     it 'test if can_be_archived? returns true' do
-#       book.move_to_archive
-#       expect(book.archived).to eq(true)
-#     end
-#   end
-# end
