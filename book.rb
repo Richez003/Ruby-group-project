4671842ac740715a3d9a1e0aad220d7eae48d181
richemonday@gmail.com <richemonday@gmail.com>
2022-08-26 14:24:26 +0100

    added json file data

--- book.rb
+++ book.rb
@@ -1,10 +1,16 @@
-require './item'
+require_relative 'item'
 
 class Book < Item
-  super()
+  attr_accessor :title, :publisher, :cover_state
 
-  publisher
-  cover_state
+  def initialize(title, publisher, cover_state, publish_date)
+    super(publish_date, archived)
+    @title = title
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  private
 
   def can_be_archived?
     super || @cover_state == 'bad'
