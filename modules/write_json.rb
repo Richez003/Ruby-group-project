c0faeaa6213f61a8ff371a574d9ca8e630411e76
Talha <tkz9696@gmail.com>
2022-08-27 18:35:31 +0500

    Add write methods template

--- modules/write_json.rb
+++ modules/write_json.rb
@@ -19,13 +19,43 @@ module WriteData
     File.write('./data/authors.json', JSON.generate(json_authors))
   end
 
-  def write_books(books) end
+  def write_books(books)
+    json_books = []
+    books.each do |book|
+      json_books << {}
+    end
+    File.write('./data/books.json', JSON.generate(json_books))
+  end
 
-  def write_games(games) end
+  def write_games(games)
+    json_games = []
+    games.each do |game|
+      json_games << {}
+    end
+    File.write('./data/authors.json', JSON.generate(json_games))
+  end
 
-  def write_genres(genres) end
+  def write_genres(genres)
+    json_genres = []
+    genres.each do |genre|
+      json_genres << {}
+    end
+    File.write('./data/authors.json', JSON.generate(json_genres))
+  end
 
-  def write_labels(labels) end
+  def write_labels(labels)
+    json_labels = []
+    labels.each do |label|
+      json_labels << {}
+    end
+    File.write('./data/authors.json', JSON.generate(json_labels))
+  end
 
-  def write_music_albums(music_albums) end
+  def write_music_albums(music_albums)
+    json_music_albums = []
+    music_albums.each do |music_album|
+      json_music_albums << {}
+    end
+    File.write('./data/authors.json', JSON.generate(json_music_albums))
+  end
 end
