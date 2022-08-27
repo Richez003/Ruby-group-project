2520f2fc0aca245e91e4fb84a6a061d2a1aee71c
Talha <tkz9696@gmail.com>
2022-08-27 18:41:32 +0500

    Game and Books write methods

--- modules/write_json.rb
+++ modules/write_json.rb
@@ -22,40 +22,49 @@ module WriteData
   def write_books(books)
     json_books = []
     books.each do |book|
-      json_books << {}
+      json_books << {
+        title: book.title,
+        publisher: book.publisher,
+        cover_state: book.cover_state,
+        publish_date: book.publish_date
+      }
     end
     File.write('./data/books.json', JSON.generate(json_books))
   end
 
   def write_games(games)
     json_games = []
-    games.each do |game|
-      json_games << {}
+    games.each do |_game|
+      json_games << {
+        multiplayer: game.multiplayer,
+        last_played_at: game.last_played_at,
+        game_publish_date: game.publish_date
+      }
     end
-    File.write('./data/authors.json', JSON.generate(json_games))
+    File.write('./data/games.json', JSON.generate(json_games))
   end
 
   def write_genres(genres)
     json_genres = []
-    genres.each do |genre|
+    genres.each do |_genre|
       json_genres << {}
     end
-    File.write('./data/authors.json', JSON.generate(json_genres))
+    File.write('./data/genres.json', JSON.generate(json_genres))
   end
 
   def write_labels(labels)
     json_labels = []
-    labels.each do |label|
+    labels.each do |_label|
       json_labels << {}
     end
-    File.write('./data/authors.json', JSON.generate(json_labels))
+    File.write('./data/labels.json', JSON.generate(json_labels))
   end
 
   def write_music_albums(music_albums)
     json_music_albums = []
-    music_albums.each do |music_album|
+    music_albums.each do |_music_album|
       json_music_albums << {}
     end
-    File.write('./data/authors.json', JSON.generate(json_music_albums))
+    File.write('./data/music_albums.json', JSON.generate(json_music_albums))
   end
 end
